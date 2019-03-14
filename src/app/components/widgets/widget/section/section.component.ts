8b76dfe400862cbe4af4a8aee01a149e915f25d8
alex.k <alex.k>
2019-03-14 22:08:05 +0200

    section component

--- src/app/components/widgets/widget/section/section.component.ts
+++ src/app/components/widgets/widget/section/section.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Section} from '../../../../core/domain/widget';
+import {Control, Section} from '../../../../core/domain/widget';
 
 @Component({
   selector: 'app-section',
@@ -8,12 +8,29 @@ import {Section} from '../../../../core/domain/widget';
 })
 export class SectionComponent implements OnInit {
   @Input() section: Section;
-
+  controlColumns: Array[Control[]];
   constructor() { }
 
   ngOnInit() {
     console.log('Section initialized: ');
     console.log(this.section);
+    this.splitByColumns();
+  }
+  private splitByColumns() {
+    const columns = this.section.columns;
+    const controls = this.section.items;
+    const controlCount = controls.length;
+    if (controls && columns) {
+      const chunkSize = Math.ceil(controlCount / columns);
+      this.controlColumns = this.splitControlsArrayIntoChunks(controls, chunkSize);
+    }
   }
 
+  private splitControlsArrayIntoChunks(controls: Array[Control], chunkSize: number): Array[Control[]] {
+    const res: Array[Section[]] = [];
+    for (let idx = 0, len = controls.length; idx < len; idx += chunkSize) {
+      res.push(controls.slice(idx, idx + chunkSize));
+    }
+    return res;
+  }
 }
